2136676047d05110c763aed71e23292c9b415af0
Pedja <vpedja12@gmail.com>
2019-11-22 15:44:14 -0800

    edited constructors

--- Develop/app.js
+++ Develop/app.js
@@ -1,33 +1,46 @@
+
 // The first class is an `Employee` parent class with the following properties and
 // methods:
-const employee = class Employee {
-this.name = name;
-this.id = id;
-this.title = title;
-getName();
-getId();
-getEmail();
-getRole(); // Returns 'Employee'
+class Employee {
+    constructor(name, id, title) {
+        this.name = name,
+            this.id = id,
+            this.title = title,
+}
+    getName(),
+
+    getId(),
+
+    getEmail(),
+
+    getRole(), // Returns 'Employee'
 };
 
 // In addition to `Employee`'s properties and methods, `Manager` will also have:
-const manager = class Manager {
-    this.officeNumber = officeNumber
-getRole()
+class Manager extends Employee {
+    constructor(officeNumber) {
+        this.officeNumber = officeNumber,
+    }
+
+    getRole(),
 };
 
 // In addition to `Employee`'s properties and methods, `Engineer` will also have:
-const engineer = class Engineer {
-    this.github = github,
+class Engineer extends Employee {
+    constructor(github) {
+        this.github = github,
+    }
     getGithub(),
     getRole(), // Overridden to return 'Engineer'
 };
 
 // In addition to `Employee`'s properties and methods, `Intern` will also have:
-const intern = class Intern {
-    this.school = school;
-getSchool();
-getRole(); // Overridden to return 'Intern'
+class Intern {
+    constructor(school) {
+        this.school = school,
+    }
+    getSchool(),
+    getRole(), // Overridden to return 'Intern'
 }
 // ### User input
 
@@ -39,9 +52,6 @@ getRole(); // Overridden to return 'Intern'
 // The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
 
 //   * Name
-
 //   * Role
-
 //   * ID
-
 //   * Role-specific property (School, link to GitHub profile, or office number)
